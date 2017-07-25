b84d9c54aff1bebddb73c9b4ec1302af540d7085
Kees Postma <kk.postma@gmail.com>
2017-07-25 09:35:22 +0100

    Adds geocoding logic to flat model

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,6 @@
 class Flat < ApplicationRecord
+  # This says what column the non-geocoded address is stored in
+  geocoded_by :address
+  # This says to geocode the address if it has changed
+  after_validation :geocode, if: :address_changed?
 end
